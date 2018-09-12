15fdb8f5c9a3af3519e2e16064de7ebbb1c59668
Markandayan Perumandi <marklevy1518@gmail.com>
2018-09-12 11:45:31 +0530

    Initial commit ( Hello world program )

---
+++ Objective-C demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C demo
+//
+//  Created by Markandayan Perumandi on 12/09/18.
+//  Copyright © 2018 Markandayan Perumandi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
