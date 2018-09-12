0080ab1483a3d7afed0832be3317b8f57de7be7b
Markandayan Perumandi <marklevy1518@gmail.com>
2018-09-12 11:55:11 +0530

    simple if condition ( to find even or odd number )

--- Objective-C demo/main.m
+++ Objective-C demo/main.m
@@ -8,10 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int a = 20;
+        if (a%2 == 0) {
+            NSLog(@"%i is EVEN number", a);
+        }else{
+            NSLog(@"%i is ODD number", a);
+        }
     }
     return 0;
 }
