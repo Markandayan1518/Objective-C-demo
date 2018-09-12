ead0fb7fa5ab7618cd884af05a7ea5c902629f79
Markandayan Perumandi <marklevy1518@gmail.com>
2018-09-12 12:00:07 +0530

    Function ( to check given number is even or odd number )

--- Objective-C demo/main.m
+++ Objective-C demo/main.m
@@ -9,14 +9,18 @@
 #import <Foundation/Foundation.h>
 
 
+static BOOL checkNumber(int a) {
+    BOOL isEven = NO;
+    if (a%2 == 0) {
+        isEven = YES;
+    }
+    return isEven;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int a = 20;
-        if (a%2 == 0) {
-            NSLog(@"%i is EVEN number", a);
-        }else{
-            NSLog(@"%i is ODD number", a);
-        }
+        int a = 25;
+        checkNumber(a) ? NSLog(@"Given number : %i is EVEN number", a) : NSLog(@"Given number : %i is ODD number", a);
     }
     return 0;
 }
